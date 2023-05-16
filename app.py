2b0e856b101922b0a0bb603dc9dacb75e5a26999
Alessandro Barbaro <barbaro222alessandro@gmail.com>
2023-05-16 11:43:35 +0200

    last

--- app.py
+++ app.py
@@ -1,13 +1,4 @@
-import numpy as np
-import pandas as pd
-import seaborn as sns
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
-from sklearn.model_selection import train_test_split
 import streamlit as st
-import plotly.express as px
-import matplotlib.pyplot as plt
-import io
 import joblib
 
 
